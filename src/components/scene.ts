0a2f5415f91785f1e0b35b8d265a8cae95890fdb
Fabricio S A <fabriciodossa@hotmail.com>
2017-10-12 10:49:27 -0300

    adding board to scene

--- src/components/scene.ts
+++ src/components/scene.ts
@@ -1,6 +1,6 @@
 // three.js
 import * as THREE from 'three'
-
+import { Grid, Board } from './components'
 // create the scene
 export class Scene {
     public title: string;
@@ -9,7 +9,7 @@ export class Scene {
     public renderer: THREE.WebGLRenderer;
     public innerWidth: number;
     public innerHeight: number;
-
+    board: Board;
     constructor(config: any) {
         this.title = config.title;
         this.innerHeight = config.innerHeight | 500;
@@ -17,12 +17,20 @@ export class Scene {
         this.camera = new THREE.PerspectiveCamera(75, this.innerWidth / this.innerHeight, 0.1, 1000);
         this.camera.position.z = 5;
         this.scene = new THREE.Scene();
+
+        var grid = new Grid({
+            rings: 5,
+            cellSize: 10
+        });
+        this.board = new Board(grid);
+
+        this.scene.add(this.board.group);
     }
 
     public setRender(container: HTMLElement) {
         this.renderer = new THREE.WebGLRenderer({ antialias: false });
         this.renderer.setSize(this.innerWidth, this.innerHeight);
-        container.appendChild( this.renderer.domElement );
+        container.appendChild(this.renderer.domElement);
     }
     public animate = () => {
         requestAnimationFrame(this.animate);
